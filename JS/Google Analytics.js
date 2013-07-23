3b8787e5ac1a8ef55d701b3be692b2e0c1bd577e
Mathias Ose <m@thiaso.se>
2013-07-23 23:02:48 +0200

    I borked it

--- JS/Google Analytics.js
+++ JS/Google Analytics.js
@@ -3,7 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-$(document).ready( function () {
-	ga('create', 'UA-40375801-1', 'thiaso.se');
-	ga('send', 'pageview');
-)};
+ga('create', 'UA-40375801-1', 'thiaso.se');
+ga('send', 'pageview');
