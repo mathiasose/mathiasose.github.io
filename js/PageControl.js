3f6e51c907aac61b3cb52446b7a5493b90fa3af8
Mathias Ose <mathias.ose@gmail.com>
2013-09-08 00:59:40 +0200

    prevent button mashing

--- js/PageControl.js
+++ js/PageControl.js
@@ -5,15 +5,30 @@ var fadeTime = 1500;
 function PageControl($scope) {
 
     $scope.changeView = function() {
-        $('#footer').fadeOut(fadeTime);
-        if ( cv_visible ) {
-            $('#changeView').html("View CV");
-            $('#cv').fadeOut(fadeTime);
-        } else {
-            $('#changeView').html("Hide CV");
-            $('#cv').fadeIn(fadeTime);
+        var btn = $('#changeView');
+        var footer = $('#footer');
+        var cv = $('#cv');
+
+        if (!btn.data('isClicked')) {
+            btn.data('isClicked', true);
+            btn.attr("disabled", true);
+            setTimeout(function() {
+                btn.removeData('isClicked')
+                btn.attr("disabled", false);
+            }, 1500);
+            footer.fadeOut(fadeTime);
+
+            if ( cv_visible ) {
+                btn.html("View CV");
+                cv.fadeOut(fadeTime);
+            } else {
+                btn.html("Hide CV");
+                cv.fadeIn(fadeTime);
+            }
+
+            footer.fadeIn(fadeTime);
+            cv_visible = !cv_visible;
         }
-        $('#footer').fadeIn(fadeTime);
-        cv_visible = !cv_visible;
     };
+
 }
