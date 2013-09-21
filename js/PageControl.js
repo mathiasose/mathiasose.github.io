82291d9a068d4442afcfbb06a019c336f8fd0499
Mathias Ose <mathias.ose@gmail.com>
2013-09-21 17:33:29 +0200

    using a JS dict and angular expressions instead of jQuery DOM manipulation

--- js/PageControl.js
+++ js/PageControl.js
@@ -1,66 +1,32 @@
 function PageControl($scope,$location,$http,$window) {
 
-    var lang_resource = null;
-    $http.get('js/lang.json')
-        .success(function (data) {
-            lang_resource = data;
-            langSwap(lang_resource);
-        });
-
+    var en_dict = {};
+    var no_dict = {};
     var window_language = $window.navigator.userLanguage || $window.navigator.language;
 
-    var cur_lang = ( window_language === 'nb-NO' || window_language === 'nn-NO' ) ? 'no' : 'en';
-
-    var correctButton = function() {
-        if ( $location.path() == '/cv' )
-            $('#changeView_closed' ).attr("id","changeView_open");
-        else
-            $('#changeView_open' ).attr("id","changeView_closed");
-    };
-
-    var langSwap = function(arg) {
-        $.each(arg, function() {
-            var id = $('#' + this.id);
-            if ( cur_lang === 'no' )
-                id.html(this.no);
-            else
-                id.html(this.en);
+    $http.get('js/lang.json')
+        .success(function (data) {
+            $.each(data, function() {
+                en_dict[this.id] = this.en;
+                no_dict[this.id] = this.no;
+            });
         });
-    };
 
-	var changeLang = function( arg ) {
-        if ( arg == 'other' )
-            cur_lang = ( cur_lang == 'en' ) ? 'no' : 'en';
-        else
-            cur_lang = arg;
+    $scope.d = (window_language === 'nb-NO' || window_language === 'nn-NO') ? no_dict : en_dict;
 
-        if ( lang_resource ) {
-            langSwap(lang_resource);
-        } else {
-            $http.get('js/lang.json')
-                .success(function (data) {
-                    lang_resource = data;
-                    langSwap(lang_resource);
-                });
-        }
-	};
-
-    var changeView = function() {
-        if ( $location.path() == '/cv' )
+    $scope.changeView = function() {
+        if ($location.path() == '/cv') {
             $location.path('');
-        else
+            en_dict["changeView"] = "View CV";
+            no_dict["changeView"] = "Se CV";
+        } else {
             $location.path('cv');
-        correctButton();
-        changeLang(cur_lang);
+            en_dict["changeView"] = "Close CV";
+            no_dict["changeView"] = "Lukk CV";
+        }
     };
 
-    // give the scope access to the functions it needs
-    $scope.changeLang = changeLang;
-    $scope.changeView = changeView;
-
-    // call these functions once after page load to ensure the page content is correct
-    $(document).ready( function() {
-        changeLang(cur_lang);
-        correctButton();
-    });
+    $scope.changeLang = function(arg) {
+        $scope.d = ($scope.d == en_dict) ? no_dict : en_dict;
+    };
 }
